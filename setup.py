6271985171622e02d372cd32e71ae95730a44189
Nick Haynes <nickdavidhaynes@gmail.com>
2017-05-26 16:21:58 -0400

    make setup.py pep8-compliant

--- setup.py
+++ setup.py
@@ -3,25 +3,27 @@ import re
 from setuptools import setup
 
 with open('wordsmith/__init__.py', 'r') as fd:
-  version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(),
+                        re.MULTILINE).group(1)
 
 if not version:
-  raise RuntimeError('Could not locate version information')
+    raise RuntimeError('Could not locate version information')
 
 setup(
-  name = 'wordsmith',
-  version = version,
-  description = 'A wrapper around the Wordsmith API written in python',
-  author = 'John Hegele - Automated Insights',
-  packages = ['wordsmith'],
-  package_dir = {'wordsmith' : 'wordsmith'},
-  install_requires = ['requests', 'six'],
-  classifiers = [
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'Natural Language :: English',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.5'
-  ]
+    name='wordsmith',
+    version=version,
+    description='A wrapper around the Wordsmith API written in python',
+    author='John Hegele - Automated Insights',
+    packages=['wordsmith'],
+    package_dir={'wordsmith': 'wordsmith'},
+    install_requires=['requests', 'six'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5'
+    ]
 )
